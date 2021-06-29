f87020a7f5a7babfc53b8846f54846e38119eadb
Devanshu sharma <84975020+devanshu045@users.noreply.github.com>
2021-06-29 23:44:13 +0530

    Add files via upload

---
+++ main/src/components/nav.js
@@ -0,0 +1,25 @@
+// export default class Npp extends Component {
+//     state = {
+//         toggle:false
+//     }
+//     Toggle = () => {
+//         this.setState({toggle:!this.state.toggle})
+//     }
+//     render() {
+//         return (
+//           <>
+//               <div className="navBar">
+//                     <button onClick={this.Toggle}>
+//                         <FaAlignRight />
+//                     </button>
+//                     <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
+//                         <li href="#">Home</li>
+//                         <li href="#">About us</li>
+//                         <li href="#">Contact</li>
+//                     </ul>
+//               </div>
+//           </>
+//         );
+//     }
+// }
+// export default Npp;
